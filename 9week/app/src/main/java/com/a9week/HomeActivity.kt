a054688d60569c1d0db08c2d23ec1052b1df5a68
박진성 <plashdof@gmail.com>
2023-06-12 04:48:55 +0900

    9week 네이버 로그인 구현

--- 9week/app/src/main/java/com/a9week/HomeActivity.kt
+++ 9week/app/src/main/java/com/a9week/HomeActivity.kt
@@ -10,6 +10,11 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.kakao.sdk.auth.AuthApiClient
 import com.kakao.sdk.common.model.KakaoSdkError
 import com.kakao.sdk.user.UserApiClient
+import com.navercorp.nid.NaverIdLoginSDK
+import com.navercorp.nid.oauth.NidOAuthLogin
+import com.navercorp.nid.oauth.OAuthLoginCallback
+import com.navercorp.nid.profile.NidProfileCallback
+import com.navercorp.nid.profile.data.NidProfileResponse
 
 class HomeActivity : AppCompatActivity() {
 
@@ -23,99 +28,189 @@ class HomeActivity : AppCompatActivity() {
         if( intent.hasExtra("kakao")){
 
             binding.btnLogout.setOnClickListener {
-                // 카카오 로그아웃
-                UserApiClient.instance.logout { error ->
-                    if (error != null) {
-                        Log.e(TAG, "로그아웃 실패. SDK에서 토큰 삭제됨", error)
-                    }
-                    else {
-                        Log.d(TAG, "로그아웃 성공. SDK에서 토큰 삭제됨")
-                        val intent = Intent(this,MainActivity::class.java)
-                        startActivity(intent)
-                    }
-                }
+                kakaoLogout()
             }
 
-            binding.btnDelete.setOnClickListener {
-                // 카카오 연결 끊기
-                UserApiClient.instance.unlink { error ->
-                    if (error != null) {
-                        Log.e(TAG, "연결 끊기 실패", error)
-                    }
-                    else {
-                        Log.d(TAG, "연결 끊기 성공. SDK에서 토큰 삭제 됨")
-                        val intent = Intent(this,MainActivity::class.java)
-                        startActivity(intent)
-                    }
-                }
+            binding.btnUnlink.setOnClickListener {
+                kakaoUnlink()
             }
 
-
-            // 토큰 유효성 검사
-            if (AuthApiClient.instance.hasToken()) {
-                UserApiClient.instance.accessTokenInfo { _, error ->
-                    if (error != null) {
-                        if (error is KakaoSdkError && error.isInvalidTokenError()) {
-                            Log.d(TAG,"로그인 필요")
-                        }
-                        else {
-                            Log.d(TAG,"기타 에러")
-                        }
-                    }
-                    else {
-                        Log.d(TAG,"토큰 유효성 체크 성공")
-                    }
-                }
-            }
-            else {
-                Log.d(TAG,"로그인 필요")
-            }
-
-
-            // 로그인 유저정보 불러오기
-            UserApiClient.instance.me { user, error ->
-                if (error != null) {
-                    Log.e(TAG, "사용자 정보 요청 실패 $error")
-                } else if (user != null) {
-                    Log.d(TAG, "사용자 정보 요청 성공 : $user")
-                    val nickname = user.kakaoAccount?.profile?.nickname
-                    val birthday = user.kakaoAccount?.birthday
-                    val email = user.kakaoAccount?.email
-                    val age = user.kakaoAccount?.ageRange.toString()
-                    binding.tvInfo.text = nickname + "\n" +birthday + "\n" + email + "\n" + age
-                }
-            }
+            kakaoCheckToken()
+            kakaoCallInfo()
 
         }else if(intent.hasExtra("google")){
 
-
-            val account = GoogleSignIn.getLastSignedInAccount(this)
-            val email = account?.email.toString()
-            val familyName = account?.familyName.toString()
-            val givenName = account?.givenName.toString()
-            val displayName = account?.displayName.toString()
-            val photoUrl = account?.photoUrl.toString()
-            val id = account?.id.toString()
-            val idToken = account?.idToken
-
-            binding.tvInfo.text = displayName + "\n" + email + "\n" + id
-
+            googleCallInfo()
             binding.btnLogout.setOnClickListener{
-                // 구글 로그아웃
-                val googleSignInClient = GoogleSignIn.getClient(this@HomeActivity, GoogleSignInOptions.Builder(
-                    GoogleSignInOptions.DEFAULT_SIGN_IN).build())
-                googleSignInClient.signOut().addOnCompleteListener {
-                    Log.d(TAG,"구글로그인 로그아웃")
-                    val intent = Intent(this,MainActivity::class.java)
-                    startActivity(intent)
-                }
+                googleLogout()
             }
 
+        } else if(intent.hasExtra("naver")){
+
+            naverCallInfo()
+
+            binding.btnLogout.setOnClickListener {
+                naverLogout()
+            }
+
+            binding.btnUnlink.setOnClickListener {
+                naverUnlink()
+            }
         }
-
-
-
-
-
     }
+
+    // 카카오 로그아웃
+    private fun kakaoLogout(){
+        UserApiClient.instance.logout { error ->
+            if (error != null) {
+                Log.e(TAG, "로그아웃 실패. SDK에서 토큰 삭제됨", error)
+            }
+            else {
+                Log.d(TAG, "로그아웃 성공. SDK에서 토큰 삭제됨")
+                val intent = Intent(this,MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
+
+    // 카카오 연결 끊기
+    private fun kakaoUnlink(){
+
+        UserApiClient.instance.unlink { error ->
+            if (error != null) {
+                Log.e(TAG, "연결 끊기 실패", error)
+            }
+            else {
+                Log.d(TAG, "연결 끊기 성공. SDK에서 토큰 삭제 됨")
+                val intent = Intent(this,MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
+    // 카카오 토큰 유효성 검사
+    private fun kakaoCheckToken(){
+        // 토큰 유효성 검사
+        if (AuthApiClient.instance.hasToken()) {
+            UserApiClient.instance.accessTokenInfo { _, error ->
+                if (error != null) {
+                    if (error is KakaoSdkError && error.isInvalidTokenError()) {
+                        Log.d(TAG,"로그인 필요")
+                    }
+                    else {
+                        Log.d(TAG,"기타 에러")
+                    }
+                }
+                else {
+                    Log.d(TAG,"토큰 유효성 체크 성공")
+                }
+            }
+        }
+        else {
+            Log.d(TAG,"로그인 필요")
+        }
+    }
+
+
+    // 카카오 유저정보 불러오기
+    private fun kakaoCallInfo(){
+        // 로그인 유저정보 불러오기
+        UserApiClient.instance.me { user, error ->
+            if (error != null) {
+                Log.e(TAG, "사용자 정보 요청 실패 $error")
+            } else if (user != null) {
+                Log.d(TAG, "사용자 정보 요청 성공 : $user")
+                val nickname = user.kakaoAccount?.profile?.nickname
+                val birthday = user.kakaoAccount?.birthday
+                val email = user.kakaoAccount?.email
+                val age = user.kakaoAccount?.ageRange.toString()
+                binding.tvInfo.text = nickname + "\n" +birthday + "\n" + email + "\n" + age
+            }
+        }
+    }
+
+
+
+    // 구글 로그아웃
+    private fun googleLogout(){
+        val googleSignInClient = GoogleSignIn.getClient(this@HomeActivity, GoogleSignInOptions.Builder(
+            GoogleSignInOptions.DEFAULT_SIGN_IN).build())
+        googleSignInClient.signOut().addOnCompleteListener {
+            Log.d(TAG,"구글로그인 로그아웃")
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    // 구글 유저정보 불러오기
+    private fun googleCallInfo(){
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        val email = account?.email.toString()
+        val familyName = account?.familyName.toString()
+        val givenName = account?.givenName.toString()
+        val displayName = account?.displayName.toString()
+        val photoUrl = account?.photoUrl.toString()
+        val id = account?.id.toString()
+        val idToken = account?.idToken
+
+        binding.tvInfo.text = displayName + "\n" + email + "\n" + id
+    }
+
+    // 네이버 로그아웃
+    private fun naverLogout(){
+        NaverIdLoginSDK.logout()
+        val intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    // 네이버 연결끊기
+    private fun naverUnlink(){
+        NidOAuthLogin().callDeleteTokenApi(this, object : OAuthLoginCallback {
+            override fun onSuccess() {
+                //서버에서 토큰 삭제에 성공한 상태입니다.
+                val intent = Intent(this@HomeActivity,MainActivity::class.java)
+                startActivity(intent)
+            }
+            override fun onFailure(httpStatus: Int, message: String) {
+                // 서버에서 토큰 삭제에 실패했어도 클라이언트에 있는 토큰은 삭제되어 로그아웃된 상태입니다.
+                // 클라이언트에 토큰 정보가 없기 때문에 추가로 처리할 수 있는 작업은 없습니다.
+                Log.d("naver", "errorCode: ${NaverIdLoginSDK.getLastErrorCode().code}")
+                Log.d("naver", "errorDesc: ${NaverIdLoginSDK.getLastErrorDescription()}")
+            }
+            override fun onError(errorCode: Int, message: String) {
+                // 서버에서 토큰 삭제에 실패했어도 클라이언트에 있는 토큰은 삭제되어 로그아웃된 상태입니다.
+                // 클라이언트에 토큰 정보가 없기 때문에 추가로 처리할 수 있는 작업은 없습니다.
+                onFailure(errorCode, message)
+            }
+        })
+    }
+
+    // 네이버 유저정보 가져오기
+    private fun naverCallInfo(){
+        NidOAuthLogin().callProfileApi(profileCallback)
+    }
+
+
+    // 네이버 유저정보 콜백
+    private val profileCallback = object : NidProfileCallback<NidProfileResponse> {
+        override fun onSuccess(response: NidProfileResponse) {
+            val id = response.profile?.id
+            val name = response.profile?.name
+            val nick = response.profile?.nickname
+            val age = response.profile?.age
+            val email = response.profile?.email
+            val birthYear = response.profile?.birthYear
+
+            binding.tvInfo.text = name + "\n" + email + "\n" + id + "\n" + nick + "\n" + age + "\n" + birthYear
+        }
+        override fun onFailure(httpStatus: Int, message: String) {
+            val errorCode = NaverIdLoginSDK.getLastErrorCode().code
+            val errorDescription = NaverIdLoginSDK.getLastErrorDescription()
+        }
+        override fun onError(errorCode: Int, message: String) {
+            onFailure(errorCode, message)
+        }
+    }
+
 }
--- 9week/app/src/main/java/com/a9week/MainActivity.kt
+++ 9week/app/src/main/java/com/a9week/MainActivity.kt
@@ -19,12 +19,20 @@ import com.kakao.sdk.common.model.ClientError
 import com.kakao.sdk.common.model.ClientErrorCause
 import com.kakao.sdk.common.util.Utility
 import com.kakao.sdk.user.UserApiClient
+import com.navercorp.nid.NaverIdLoginSDK
+import com.navercorp.nid.oauth.NidOAuthLogin
+import com.navercorp.nid.oauth.OAuthLoginCallback
+import com.navercorp.nid.profile.NidProfileCallback
+import com.navercorp.nid.profile.data.NidProfileResponse
 
 class MainActivity : AppCompatActivity() {
 
     val TAG = "debugging"
     private lateinit var binding : ActivityMainBinding
     private lateinit var resultLauncher : ActivityResultLauncher<Intent>
+    val naverClientId = "HnoNyYn8Uhy_l5QZ1s4i"
+    val naverClientSecret = "JUqGLXLRDf"
+    val naverClientName = "네이버 로그인"
 
 
 
@@ -34,45 +42,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         setResultSignUp()
 
+        // 네이버 로그인
+        NaverIdLoginSDK.initialize(this,naverClientId,naverClientSecret,naverClientName)
+        binding.btnNaver.setOnClickListener {
+            naverLogin()
+        }
 
         // 카카오 로그인
-        
         Log.d(TAG, "keyhash : ${Utility.getKeyHash(this)}")
         KakaoSdk.init(this, "838fc4e08a57b548debcba38b943a928")
         binding.btnKakao.setOnClickListener {
-            // 카카오톡 설치 확인
-            if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
-                // 카카오톡 로그인
-                UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
-
-                    // 로그인 실패 부분
-                    if (error != null) {
-                        Log.e(TAG, "앱 로그인 실패 $error")
-                        // 사용자가 취소
-                        if (error is ClientError && error.reason == ClientErrorCause.Cancelled ) {
-                            return@loginWithKakaoTalk
-                        }
-                        // 다른 오류
-                        else {
-                            UserApiClient.instance.loginWithKakaoAccount(this, callback = mCallback) // 카카오 이메일 로그인
-                        }
-                    }
-                    // 로그인 성공 부분
-                    else if (token != null) {
-                        Log.d(TAG, "앱 로그인 성공 ${token.accessToken}")
-                        val intent = Intent(this,HomeActivity::class.java)
-                            .putExtra("kakao","kakao")
-                        startActivity(intent)
-                    }
-                }
-            } else {
-                UserApiClient.instance.loginWithKakaoAccount(this, callback = mCallback) // 카카오 이메일 로그인
-            }
+            kakaoLogin()
         }
 
 
         // 구글 로그인
-        
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestProfile()
@@ -86,14 +70,68 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    private fun kakaoLogin(){
+        // 카카오톡 설치 확인
+        if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+            // 카카오톡 로그인
+            UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
+                // 로그인 실패 부분
+                if (error != null) {
+                    Log.e(TAG, "앱 로그인 실패 $error")
+                    // 사용자가 취소
+                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled ) {
+                        return@loginWithKakaoTalk
+                    }
+                    // 다른 오류
+                    else {
+                        UserApiClient.instance.loginWithKakaoAccount(this, callback = kakaoEmailCb) // 카카오 이메일 로그인
+                    }
+                }
+                // 로그인 성공 부분
+                else if (token != null) {
+                    Log.d(TAG, "앱 로그인 성공 ${token.accessToken}")
+                    val intent = Intent(this,HomeActivity::class.java)
+                        .putExtra("kakao","kakao")
+                    startActivity(intent)
+                }
+            }
+        } else {
+            UserApiClient.instance.loginWithKakaoAccount(this, callback = kakaoEmailCb) // 카카오 이메일 로그인
+        }
+    }
+
+    private fun naverLogin(){
+        val oauthLoginCallback = object : OAuthLoginCallback {
+            override fun onSuccess() {
+                // 네이버 로그인 인증이 성공했을 때 수행할 코드 추가
+                val intent = Intent(this@MainActivity,HomeActivity::class.java)
+                    .putExtra("naver","naver")
+                startActivity(intent)
+            }
+            override fun onFailure(httpStatus: Int, message: String) {
+                val errorCode = NaverIdLoginSDK.getLastErrorCode().code
+                val errorDescription = NaverIdLoginSDK.getLastErrorDescription()
+            }
+            override fun onError(errorCode: Int, message: String) {
+                onFailure(errorCode, message)
+            }
+        }
+        NaverIdLoginSDK.authenticate(this, oauthLoginCallback)
+    }
+
+
     // 카카오톡 이메일 로그인 콜백
-    private val mCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+    private val kakaoEmailCb: (OAuthToken?, Throwable?) -> Unit = { token, error ->
         if (error != null) {
             Log.e(TAG, "이메일 로그인 실패 $error")
         } else if (token != null) {
             Log.d(TAG, "이메일 로그인 성공 ${token.accessToken}")
+            val intent = Intent(this,HomeActivity::class.java)
+                .putExtra("kakao","kakao")
+            startActivity(intent)
         }
     }
+
 
     // 구글로그인 콜백
     private fun setResultSignUp(){
